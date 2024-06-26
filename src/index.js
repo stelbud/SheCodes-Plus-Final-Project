473eaccc64b25d3bb4c655c3dfbc9412ed6ac6f9
stelbud <stellabudi@hotmail.com>
2024-06-26 14:35:06 +0700

    Change city name using JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("hello world");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
