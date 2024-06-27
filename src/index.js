7a87539f8866a9c7c33e2840571167a8b2575b6f
stelbud <stellabudi@hotmail.com>
2024-06-27 14:36:02 +0700

    Create function searchCity to make an API call then update the interface

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+function refreshWeather(response) {
+  console.log(response.data);
+}
+
+function searchCity(city) {
+  let apiKey = "683ca4t0db1024efbc065a4o1b7796e4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city-name");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
