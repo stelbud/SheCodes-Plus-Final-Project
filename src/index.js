5788363665ff2fde6ca9e64dadee8308b2d6f0f5
stelbud <stellabudi@hotmail.com>
2024-06-27 15:29:56 +0700

    Inject real temperature of Osaka when refreshing the page

--- src/index.js
+++ src/index.js
@@ -22,3 +22,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Osaka");
